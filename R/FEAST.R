22fdb250d9f868bda6aecea77f0e6dc1a6a8c9f3
bridgeta <15991266+bridgeta@users.noreply.github.com>
2020-07-13 11:34:17 +1200

    Spell check
    correct spelling
    alter 'sprintf' message to return number of non-matches

--- R/FEAST.R
+++ R/FEAST.R
@@ -4,7 +4,7 @@
 #' a count matrix \eqn{C}  of size \eqn{m} by \eqn{n} and a metadata table, of size \eqn{m} by \eqn{3},
 #' where \eqn{m} is the number samples and \eqn{n} is the number of taxa.
 #' The rownames of both \eqn{C} and the metadata table are the unique sample ids.
-
+#' 'id' must also be a variable in the metadata table.
 
 #' @param C An \eqn{m} by \eqn{n} count matrix, where \eqn{m} is the number samples and \eqn{n} is the number of taxa.
 #' @param metadata An \eqn{m} by \eqn{3} table, where \eqn{m} is the number samples.
@@ -18,14 +18,14 @@
 #' @param EM_iterations A numeric value indicating the number of EM iterations (default 1000).
 #' @param COVERAGE A numeric value indicating the rarefaction depth (default = minimal sequencing depth within each group of sink
 #' and its corresponding sources).
-#' @param different_sources_flag A boolian value indicating the source-sink assignment.
+#' @param different_sources_flag A boolean value indicating the source-sink assignment.
 #' different_sources_flag = 1 if different sources are assigned to each sink , otherwise = 0.
 #' @param dir_path A path to an output .txt file.
 #' @param outfile the prefix for saving the output file.
 #' @return P - an \eqn{S1} by \eqn{S2} matrix, where \eqn{S1} is the number sinks and \eqn{S2}
 #' is the number of sources (including an unknown source). Each row in matrix \eqn{P} sums to 1.
 #' \eqn{Pij} is the contribution of source j to sink i.
-#' If \eqn{Pij} == NA it indicateds that source j was not used in the analysis of sink i.
+#' If \eqn{Pij} == NA it indicates that source j was not used in the analysis of sink i.
 #'
 #' @examples
 #' \donttest{
@@ -52,7 +52,7 @@ FEAST <- function(C
 
   ###1. Parse metadata and check it has the correct hearer (i.e., Env, SourceSink,	id)
   if(sum(colnames(metadata)=='Env')==0) stop("The metadata file must contain an 'Env' column naming the source environment for each sample.")
-  if(sum(colnames(metadata)=='SourceSink')==0) stop("The metadata file must contain a 'SourceSink' column indicating 'source' or 'sink' for each sample.")
+  if(sum(colnames(metadata)=='SourceSink')==0) stop("The metadata file must contain a 'SourceSink' column indicating 'Source' or 'Sink' for each sample.")
   # produces an error where the id is not recognised as a rowname below
   if(sum(colnames(metadata)=='id')==0) stop("The metadata file must contain an 'id' column matching the source environments for each sink sample.")
   sink_ids <- grep("sink",as.character(metadata$SourceSink),ignore.case=TRUE,value=FALSE)
@@ -64,7 +64,7 @@ FEAST <- function(C
 
   ###2. Enforce integer data
   if(sum(as.integer(C) != as.numeric(C)) > 0){
-    stop('Data must be integral. Consider using "ceiling(datatable)" or ceiling(1000*datatable) to convert floating-point data to integers.')
+    stop('Data must be integer. Consider using "ceiling(datatable)" or ceiling(1000*datatable) to convert floating-point data to integers.')
   }
 
 
@@ -76,8 +76,7 @@ FEAST <- function(C
   # Double-check that the metadata file and otu table
   # had overlapping samples
   if(length(common.sample.ids) <= 1) {
-    message <- paste(sprintf('Error: there are %d sample ids in common '), # returns an ambiguous error message.
-                     'between the metadata file and data table')
+    message <- paste(sprintf('Error: there are %d sample ids in common between the metadata file and data table', common.sample.ids))
     stop(message)   
   }
   
@@ -102,11 +101,10 @@ FEAST <- function(C
   idx.unknown <- num_sources+1
 
   ###Quantify the sources' contribution for each sink sample separately
-
   for(it in 1:num_sinks){
     
     if(it%%10==0 || it == num_sinks)
-      print(paste0("Calculating mixinig proportions for sink ", it))
+      print(paste0("Calculating mixing proportions for sink ", it))
 
     ###6. Assign ids to sinks and their corresponding sources - for every sink (test.id) match its sources (train.ix)
     if(different_sources_flag == 1){
@@ -144,7 +142,7 @@ FEAST <- function(C
 
     } else{
 
-      cat(sprintf('Error: the sequencing depth in one sink or source sample in Id %d is zero. No proportions were generated\n', it))
+      cat(sprintf('Error: the sequencing depth in one sink or source sample in ID %d is zero. No proportions were generated\n', it))
 
     }
   }
